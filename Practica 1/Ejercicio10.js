49d7f79154509f462a6c7f55e18f8d3871be644e
pablo <pc416533@gmail.com>
2026-03-08 10:51:59 -0400

    subiendo

---
+++ Practica 1/Ejercicio10.js
@@ -0,0 +1,23 @@
+/*
+Cuándo es conveniente usar callbacks
+Un callback es simplemente una función que se pasa como argumento a otra función para que se ejecute después.
+
+Conviene usar callbacks cuando:
+
+1 La operación es simple y corta.
+2 Solo necesitas una acción después de otra.
+3 No hay muchas operaciones encadenadas.
+4 No necesitas manejar errores complejos.
+
+
+
+Cuándo es mejor usar promesas
+Las promesas se usan cuando el flujo asíncrono empieza a ser más complejo.
+
+Es mejor usar promesas cuando:
+
+1 Hay varias operaciones asíncronas seguidas.
+2 Necesitas manejo de errores más claro.
+3 Quieres evitar el callback hell (anidamiento excesivo).
+4 Quieres usar async/await.
+*/
